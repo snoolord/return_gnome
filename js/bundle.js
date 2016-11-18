86be18affdac7bd470678a145f88fe48641ac81e
Winston <winston.j.zhao@gmail.com>
2016-11-18 10:19:06 -0800

    typos

--- js/bundle.js
+++ js/bundle.js
@@ -72,7 +72,7 @@
 	  return [{
 	    level: 1,
 	    title: "Center",
-	    instructions: "<p>Welcome to Return Gnome! A fairy has taken control of the forest and keeps moving all the Gnomes' homes, getting them all lost! Help Fred the Gnome and his friends find their way home by writing CSS code! Use <code>justify-content</code> property to guide Fred home.</br> Justify-content aligns items horizontally and accepts the following values:</p><ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: flex-end;</code> will move the Fred to the right.</p>",
+	    instructions: "<p>Welcome to Return Gnome! A fairy has taken control of the forest and keeps moving all the Gnomes' homes, getting them all lost! Help Fred the Gnome and his friends find their way home by writing CSS code! Use <code>justify-content</code> property to guide Fred home.</br> </br> <code>justify-content</code> aligns items horizontally and accepts the following values:</p><ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: flex-end;</code> will move the Fred to the right.</p>",
 	    hint: "Use Display Flex to Center",
 	    gnomes: ['gnome.png'],
 	    houses: 1,
@@ -80,7 +80,7 @@
 	  }, {
 	    level: 2,
 	    title: "Space-between",
-	    instructions: "<p>On this level, use justify-content again to help Fred and Thomas!  Once, again justify-content aligns items horizontally and accepts the following values:</p><ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: flex-end;</code> will move the Fred to the right.</p>",
+	    instructions: "<p>On this level, use justify-content again to help Fred and Thomas!  Once, again <code>justify-content</code> aligns items horizontally and accepts the following values:</p><ul><li><code>flex-start</code>: Items align to the left side of the container.</li><li><code>flex-end</code>: Items align to the right side of the container.</li><li><code>center</code>: Items align at the center of the container.</li><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul><p>For example, <code>justify-content: flex-end;</code> will move the Fred to the right.</p>",
 	    hint: "Use justify-content: space-between",
 	    gnomes: ['gnome.png', 'gnome-brown.png'],
 	    houses: 2,
@@ -88,7 +88,7 @@
 	  }, {
 	    level: 3,
 	    title: "Space-around",
-	    instructions: "<p>On this level, we will justify-content to help Fred, Dave and Thomas! </br> The following values might be useful:</p><ul><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul>",
+	    instructions: "<p>On this level, we will <code>justify-content</code> to help Fred, Dave and Thomas! </br> The following values might be useful:</p><ul><li><code>space-between</code>: Items display with equal spacing between them.</li><li><code>space-around</code>: Items display with equal spacing around them.</li></ul>",
 	    hint: "Use justify-content: space-around",
 	    gnomes: ['gnome.png', 'gnome-purple.png', 'gnome-brown.png'],
 	    houses: 3,
@@ -136,7 +136,7 @@
 	  }, {
 	    level: 9,
 	    title: "Align-items + Rotate",
-	    instructions: "<p>This fairy is on a rampage literally putting the world on its side! Use <code>align-items</code> and <code>flex-direction</code> with <code>transform</code> to get them back home! <code>transform</code> can take:</p><ul><li><code>scale(-1)</code>: Item flips horizontally</li><li><code>rotate(Xdeg)</code>: Item rotates depending on X degrees specified</li></ul><p>Also these properties might be useful:</p><ul><li><code>center</code>: Items align in the center vertically</li><li><code>column</code> Items appear vertically in a column </li> </ul></p>",
+	    instructions: "<p>This fairy is on a rampage literally putting the world on its side! Use <code>align-items</code> and <code>flex-direction</code> with <code>transform</code> to get them back home! <code>transform</code> can take:</p><ul><li><code>scale(-1)</code>: Item flips horizontally</li><li><code>rotate(Xdeg)</code>: Item rotates depending on X degrees specified</li></ul><p>Also these properties might be useful:</p><ul><li><code>center</code>: Items align in the center vertically</li><li><code>column</code>: Items appear vertically in a column </li> </ul></p>",
 	    hint: "Use align-items: center",
 	    gnomes: ['gnome.png', 'gnome-purple.png', 'gnome-brown.png'],
 	    houses: 3,
@@ -144,7 +144,7 @@
 	  }, {
 	    level: 10,
 	    title: "Align-items + Rotate",
-	    instructions: "<p>This fairy is on a rampage literally putting the world on its side! Combine everything you learned to fight the fairy one last time! <code>align-items</code>, <code>flex-direction</code> <code>justify-content</code> ,and <code>transform</code>! Once again <code>transform</code> can take:</p><ul><li><code>scale(-1)</code>: Item flips horizontally</li><li><code>rotate(Xdeg)</code>: Item rotates depending on X degrees specified</li></ul><p>Also these properties might be useful:</p><ul><li><code>center</code>: Items align in the center vertically</li><li><code>column</code> Items appear vertically in a column </li> </ul></p>",
+	    instructions: "<p>This fairy is on a rampage literally putting the world on its side! Combine everything you learned to fight the fairy one last time! <code>align-items</code>, <code>flex-direction</code>, <code>justify-content</code>, and <code>transform</code>! Once again <code>transform</code> can take:</p><ul><li><code>scale(-1)</code>: Item flips horizontally</li><li><code>rotate(Xdeg)</code>: Item rotates depending on X degrees specified</li></ul><p>Also these properties might be useful:</p><ul><li><code>center</code>: Items align in the center vertically</li><li><code>column</code>: Items appear vertically in a column </li> </ul></p>",
 	    hint: "Use align-items: center",
 	    gnomes: ['gnome.png', 'gnome-purple.png', 'gnome-brown.png'],
 	    houses: 3,
